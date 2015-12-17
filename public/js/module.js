fd32f1819c3bfca2b84cc43da244dcb50726debe
Paul Richter <mpr75@cornell.edu>
2015-12-17 08:59:33 -0800

    update package json for heroku

--- public/js/module.js
+++ public/js/module.js
@@ -3,7 +3,8 @@
 var app = angular.module('aloudApp', ['ui.router']);
 
 app.constant('ENV', {
-  API_URL: 'http://localhost:3000'
+  API_URL: 'https://protected-cliffs-4451.herokuapp.com',
+  LOCAL_API_URL: 'http://localhost:3000'
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
